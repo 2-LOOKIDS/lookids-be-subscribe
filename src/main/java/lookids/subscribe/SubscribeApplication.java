6cee4ad45005652d1db1e7b30d67884b2c0a217f
Kimhyunjin4455 <dp4455@naver.com>
2024-12-09 17:31:30 +0900

    Feat: 게시글 알림 신청 시 중복 방지 처리 KAN-447

--- src/main/java/lookids/subscribe/SubscribeApplication.java
+++ src/main/java/lookids/subscribe/SubscribeApplication.java
@@ -2,13 +2,12 @@ package lookids.subscribe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 //@EnableCaching
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SubscribeApplication {
 
 	public static void main(String[] args) {
